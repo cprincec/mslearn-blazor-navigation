33e93919c6cec6935325c019a1f3bfcb31455863
cprincec <chukwuprincec@gmail.com>
2025-02-21 00:48:42 +0100

    completed blazing pizza

--- SpecialsController.cs
+++ SpecialsController.cs
@@ -19,4 +19,21 @@ public class SpecialsController : Controller
     {
         return (await _db.Specials.ToListAsync()).OrderByDescending(s => s.BasePrice).ToList();
     }
+
+    [HttpGet("{orderId}")]
+    public async Task<ActionResult<OrderWithStatus>> GetOrderWithStatus(int orderId)
+    {
+        var order = await _db.Orders
+            .Where(o => o.OrderId == orderId)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Special)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
+            .SingleOrDefaultAsync();
+
+        if (order == null)
+        {
+            return NotFound();
+        }
+
+        return OrderWithStatus.FromOrder(order);
+    }
 }
